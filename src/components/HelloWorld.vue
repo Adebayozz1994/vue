2b93cbdbd43aaef363ae09af9b90461f40684e7d
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-07-04 11:14:39 -0100

    new commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,88 @@
+<script setup>
+import {ref} from 'vue'
+const name = ref('adebayo');
+let num = 29;
+var school = 'university';
+
+// loop-redering
+const students =[
+  {id: 1, name: 'bayo', age: 20},
+  {id: 2, name: 'ade', age: 10},
+  {id: 3, name: 'adebayo', age: 29},
+  {id: 3, name: 'ola', age: 18},
+
+]
+const day = 'thursday';
+const age = ref(1)
+const myFunction = () =>{
+    name.value = "opeyemi"
+}
+
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+    <h1>Hello, welcome to vue 3.</h1>
+  <div>
+  </div>
+  <div>
+    <p>{{name}}</p>
+  </div>
+  
+ <!-- data-binding - one way binding -->
+  <div v-for="(student, index) in students"  v-bind:key="student.id">
+    <p>{{index + 1}} {{student.name}},  {{student.age}}</p>
+  </div>
+  <!-- conditional-statement -->
+  <div>
+  <p v-if="day == 'thursday'">Today is thursday</p> 
+  <p v-else-if="day ==='friday'">Today is friday</p>
+  <p v-else>Today is not thursday</p>
+  </div>
+  <!-- v-show -->
+  <p v-show="day === 'thursday'">Today is thursday</p>
+
+  <!-- functions and event -->
+  <button v-on:click="age++">increase age</button>
+  <p>Age- {{age}}</p>
+  <button @click="myFunction">Toggle name</button>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/indexComponent.vue
@@ -0,0 +1,49 @@
+// option API
+<template>
+    <div>
+        <h1>hello, world.</h1>
+        <p>{{username}}</p>
+        <p>{{age}}</p>
+        <p>{{msg}}</p>
+        <p>{{username + ' ' + age}}</p>
+    </div>
+    <div v-for="(student, index) in students"  v-bind:key="student.id">
+    <p>{{index + 1}} {{student.name}},  {{student.age}}</p>
+  </div>
+  <div>
+    <button @click="score++">increase score</button>
+    <p>{{score}}</p>
+    <button @click="myFunction">Toggle name</button>
+    <p>{{username}}</p>
+  </div>
+</template>
+
+
+
+<script> 
+export default {
+    setup(){
+     const name = 'adebayo';
+     return{name}
+    },
+    data() {
+        return {
+            msg: 'hello, world.',
+            username: 'adebayo',
+            age: 30,
+            students:[
+         {id: 1, name: 'bayo', age: 20},
+         {id: 2, name: 'ade', age: 10},
+         {id: 3, name: 'adebayo', age: 29},
+         {id: 3, name: 'ola', age: 18},
+          ],
+          score: 50
+        }
+    },
+    methods: {
+        myFunction(){
+            this.username = 'oyin'
+        }
+    }
+}
+</script>

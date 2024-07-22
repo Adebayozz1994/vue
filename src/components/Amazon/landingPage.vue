a1243754fbab0258fe0a2e9b02af9fb57c30d84c
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-07-22 20:50:44 -0100

    new commit

--- src/components/Amazon/landingPage.vue
+++ src/components/Amazon/landingPage.vue
@@ -25,7 +25,7 @@
         </a>
       </div>
     </nav>
-    
+
     <!-- Secondary Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" style="top: 56px;">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -67,57 +67,253 @@
         <a href="#" class="list-group-item list-group-item-action bg-light">Amazon Appstore</a>
         <hr class="bg-light">
         <h5 class="text-bold text-dark bg-light">Shop by Department</h5>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Eletronics</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Electronics</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Computers</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Smart Home</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Arts & Crafts</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">See all</a>
         <hr class="bg-light">
         <h5 class="text-bold text-dark bg-light">Programs and Features</h5>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Gifts Cards</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Shop By intrest</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Gift Cards</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Shop By Interest</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Amazon Live</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">International Shopping</a>
         <hr class="bg-light">
-        <h5 class="text-bold text-dark bg-light">Helps and Settings</h5>
+        <h5 class="text-bold text-dark bg-light">Help and Settings</h5>
         <a href="#" class="list-group-item list-group-item-action bg-light">Your Account</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">English</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">United States</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Costomer Service</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">SignIn</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Customer Service</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Sign In</a>
       </div>
     </div>
 
-    
-    
     <!-- Page Content -->
     <div id="page-content-wrapper">
       <div class="container-fluid">
         <!-- Carousel -->
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-bs-ride="carousel">
           <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="https://via.placeholder.com/800x400?text=First+Slide" alt="First slide">
+            <div class="carousel-item active" data-bs-interval="3000">
+              <img src="https://media.istockphoto.com/id/1383962772/photo/nigerian-food.jpg?s=612x612&w=0&k=20&c=u-_1qwHBoY9Yh3alnsRbix6hxgl3_TXhT9lpEvlhH10=" class="d-block w-100 img-fluid" alt="First slide">
             </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="https://via.placeholder.com/800x400?text=Second+Slide" alt="Second slide">
+            <div class="carousel-item" data-bs-interval="3000">
+              <img src="https://mamaputafrikanarestaurant.com/wp-content/uploads/2024/01/Untitled-design-4.png" class="d-block w-100 img-fluid" alt="Second slide">
             </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="https://via.placeholder.com/800x400?text=Third+Slide" alt="Third slide">
+            <div class="carousel-item" data-bs-interval="3000">
+              <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="d-block w-100 img-fluid" alt="Third slide">
             </div>
           </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
         </div>
       </div>
     </div>
   </div>
+
+<!-- cards -->
+ 
+           <div class="container my-4">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="card">
+          <img class="card-img-top" src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" alt="Deals in PCs">
+          <div class="card-body">
+            <h5 class="card-title">Deals in PCs</h5>
+            <a href="#" class="btn btn-link">Shop now</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Refresh your space</h5>
+            <div class="row">
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Dining">
+                <p>Dining</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Home">
+                <p>Home</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Kitchen">
+                <p>Kitchen</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Health and Beauty">
+                <p>Health and Beauty</p>
+              </div>
+            </div>
+            <a href="#" class="btn btn-link">See more</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Gaming accessories</h5>
+            <div class="row">
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Headsets">
+                <p>Headsets</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Keyboards">
+                <p>Keyboards</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Computer mice">
+                <p>Computer mice</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Chairs">
+                <p>Chairs</p>
+              </div>
+            </div>
+            <a href="#" class="btn btn-link">See more</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Shop deals in Fashion</h5>
+            <div class="row">
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Jeans under $50">
+                <p>Jeans under $50</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Tops under $25">
+                <p>Tops under $25</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Dresses under $30">
+                <p>Dresses under $30</p>
+              </div>
+              <div class="col-6">
+                <img src="https://i0.wp.com/ounjealadun.com/wp-content/uploads/2015/05/20170423_164717_wm.jpg?resize=1024%2C972&ssl=1" class="img-fluid" alt="Shoes under $50">
+                <p>Shoes under $50</p>
+              </div>
+            </div>
+            <a href="#" class="btn btn-link">See all deals</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- //footer -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h5>Get to Know Us</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">About Amazon</a></li>
+            <li><a href="#">Investor Relations</a></li>
+            <li><a href="#">Amazon Devices</a></li>
+            <li><a href="#">Amazon Science</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Make Money with Us</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">Sell products on Amazon</a></li>
+            <li><a href="#">Sell on Amazon Business</a></li>
+            <li><a href="#">Sell apps on Amazon</a></li>
+            <li><a href="#">Become an Affiliate</a></li>
+            <li><a href="#">Advertise Your Products</a></li>
+            <li><a href="#">Self-Publish with Us</a></li>
+            <li><a href="#">Host an Amazon Hub</a></li>
+            <li><a href="#">See More Make Money with Us</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Amazon Payment Products</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">Amazon Business Card</a></li>
+            <li><a href="#">Shop with Points</a></li>
+            <li><a href="#">Reload Your Balance</a></li>
+            <li><a href="#">Amazon Currency Converter</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Let Us Help You</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">Amazon and COVID-19</a></li>
+            <li><a href="#">Your Account</a></li>
+            <li><a href="#">Your Orders</a></li>
+            <li><a href="#">Shipping Rates & Policies</a></li>
+            <li><a href="#">Returns & Replacements</a></li>
+            <li><a href="#">Manage Your Content and Devices</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <footer class="footer bg-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-6">
+          <h5>Amazon Music</h5>
+          <p>Stream millions of songs</p>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h5>Amazon Ads</h5>
+          <p>Reach customers wherever they spend their time</p>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h5>6pm</h5>
+          <p>Score deals on fashion brands</p>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h5>AbeBooks</h5>
+          <p>Books, art & collectibles</p>
+        </div>
+        <!-- Add more columns as needed -->
+      </div>
+      <div class="row mt-3">
+        <div class="col-md-3 col-sm-6">
+          <h5>AmazonGlobal</h5>
+          <p>Ship Orders Internationally</p>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h5>Home Services</h5>
+          <p>Experienced Pros Happiness Guarantee</p>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h5>Amazon Web Services</h5>
+          <p>Scalable Cloud Computing Services</p>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h5>Audible</h5>
+          <p>Listen to Books & Original Audio Performances</p>
+        </div>
+        <!-- Add more columns as needed -->
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-12 text-center">
+          <ul class="list-inline">
+            <li class="list-inline-item"><a href="#" class="text-white">Conditions of Use</a></li>
+            <li class="list-inline-item"><a href="#" class="text-white">Privacy Notice</a></li>
+            <li class="list-inline-item"><a href="#" class="text-white">Consumer Health Data Privacy Disclosure</a></li>
+            <li class="list-inline-item"><a href="#" class="text-white">Your Ads Privacy Choices</a></li>
+          </ul>
+          <p>© 1996-2024, Amazon.com, Inc. or its affiliates</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
 </template>
 
 <script>
@@ -137,7 +333,6 @@ export default {
 </script>
 
 <style scoped>
-
 .navbar-custom {
   background-color: #0c0c0c;
 }
@@ -152,7 +347,7 @@ export default {
   justify-content: flex-end;
 }
 #sidebar {
-    background-color: #F8F9FA;
+  background-color: #F8F9FA;
   width: 250px;
   height: 100vh;
   position: fixed;
@@ -168,7 +363,7 @@ export default {
 #page-content-wrapper {
   margin-left: 0; /* No margin to keep content in place */
   width: 100%;
-  padding-top: 16px; /* Additional padding for content */
+  padding-top: 72px; /* Additional padding for content */
 }
 .close {
   position: absolute;
@@ -177,5 +372,84 @@ export default {
   background: none;
   border: none;
   font-size: 1.5rem;
+}
+.carousel-inner img {
+  width: 100%;
+  height: 75vh; /* Ensure images cover the full height of the viewport */
+  object-fit: cover; /* Cover the entire carousel area */
+}
+.carousel-item {
+  min-height: 100vh; /* Ensure carousel items cover the full height of the viewport */
+}
+
+body {
+  background-color: #232f3e;
+  color: #ffffff;
+}
+.footer {
+  padding: 20px 0;
+  background-color: #232f3e;
+  color: #ffffff;
+}
+.footer a {
+  color: #ffffff;
+}
+.footer a:hover {
+  text-decoration: underline;
+}
+.footer hr {
+  border-top: 1px solid #3a4553;
+}
+.card {
+  border: none;
+}
+.card-title {
+  font-weight: bold;
+}
+.card-body p {
+  margin-bottom: 0;
+}
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
+@media (max-width: 767px) {
+  .navbar-custom .navbar-nav .nav-link {
+    font-size: 14px;
+  }
+  #page-content-wrapper {
+    padding-top: 100px; /* Adjusted for mobile view */
+  }
+  .carousel-inner img {
+    margin-top: 150px;
+    height: 75vh; /* Adjust height for mobile view */
+  }
+  .carousel-item {
+    min-height: 50vh; /* Adjust height for mobile view */
+  }
+  .footer {
+  padding: 20px 0;
+}
+.footer h5 {
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+}
+.footer p {
+  margin-bottom: 1rem;
+}
+.footer ul {
+  padding-left: 0;
+  list-style: none;
+}
+.footer ul li a {
+  color: #ffffff;
+  text-decoration: none;
+}
+.footer ul li a:hover {
+  text-decoration: underline;
+}
+.footer hr {
+  border-top: 1px solid #3a4553;
+}
 }
 </style>
